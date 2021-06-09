e31e4ee3e821d90816e67a76ead11c7fecfc1596
David Pessanha <davidppessanha@gmail.com>
2021-06-09 19:12:44 -0300

    project cards are links now

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,13 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def show
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
 end

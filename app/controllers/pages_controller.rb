e56fa9a4378e5bc962964a971307a08461bfcec0
Pedro Barbosa <pedrovbb89@gmail.com>
2021-06-10 18:47:14 -0300

    Create_session method to initiate the pagseguro api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,13 @@ class PagesController < ApplicationController
   def download
     send_file 'app/assets/public/autorizacao-nova.doc', type: "document/doc", :x_sendfile=>true
   end
+
+  def transfer
+    create_session
+  end
+
+  def create_session
+    response = HTTParty.post("https://ws.sandbox.pagseguro.uol.com.br/v2/sessions?email=#{ENV['PAGSEGURO_EMAIL']}&token=#{ENV['PAGSEGURO_TOKEN']}")
+    @pag_session_id = response['session']['id']
+  end
 end

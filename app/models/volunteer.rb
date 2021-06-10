7e7df015316fe8a4e6b3687809aa0bb0f0ad9b5a
David Pessanha <davidppessanha@gmail.com>
2021-06-10 15:01:04 -0300

    Added some validations to marital status + associations form

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,7 @@
 class Volunteer < ApplicationRecord
   has_many :solicitations
+  MAR_STATUS = ['Solteiro(a)', 'Casado(a)', 'Separado(a)', 'Divorciado(a)', 'Viúvo(a)']
+
   validates :name, :email, :phone, :skills, :availability, :experience, presence: true
+  validates :marital_status, presence: true, inclusion: { in: MAR_STATUS }
 end

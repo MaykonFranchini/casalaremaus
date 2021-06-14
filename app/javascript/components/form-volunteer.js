617fe6d37574a7222375d741cc898e0189b8729e
David Pessanha <davidppessanha@gmail.com>
2021-06-14 20:40:39 -0300

    transformed availability into string before saving a volunteer instance

--- app/javascript/components/form-volunteer.js
+++ app/javascript/components/form-volunteer.js
@@ -2,9 +2,12 @@ const cpfOrCnpj = () => {
     
     if (document.getElementById("volunteer_cpf_or_cnpj_cpf").checked) {
       document.getElementById("cnpj-box").style.display = "none"
+      document.getElementById("volunteer_cnpj").value = ""
       document.getElementById("cpf-box").style.display = ""
+
     } else if (document.getElementById("volunteer_cpf_or_cnpj_cnpj").checked){
         document.getElementById("cpf-box").style.display = "none"
+        document.getElementById("volunteer_cpf").value = ""
         document.getElementById("cnpj-box").style.display = ""
     }
   };

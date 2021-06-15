cfe7db9731cbd8e91cafae460376f46c61300eae
David Pessanha <davidppessanha@gmail.com>
2021-06-14 23:39:31 -0300

    Added mask to cpf input

--- app/javascript/components/form-volunteer.js
+++ app/javascript/components/form-volunteer.js
@@ -1,15 +1,33 @@
 const cpfOrCnpj = () => {
-    
-    if (document.getElementById("volunteer_cpf_or_cnpj_cpf").checked) {
-      document.getElementById("cnpj-box").style.display = "none"
-      document.getElementById("volunteer_cnpj").value = ""
-      document.getElementById("cpf-box").style.display = ""
 
-    } else if (document.getElementById("volunteer_cpf_or_cnpj_cnpj").checked){
-        document.getElementById("cpf-box").style.display = "none"
-        document.getElementById("volunteer_cpf").value = ""
-        document.getElementById("cnpj-box").style.display = ""
-    }
-  };
+  if (document.getElementById("volunteer_cpf_or_cnpj_cpf").checked) {
+    document.getElementById("cnpj-box").style.display = "none"
+    document.getElementById("volunteer_cnpj").value = ""
+    document.getElementById("cpf-box").style.display = ""
+
+  } else if (document.getElementById("volunteer_cpf_or_cnpj_cnpj").checked){
+    document.getElementById("cpf-box").style.display = "none"
+    document.getElementById("volunteer_cpf").value = ""
+    document.getElementById("cnpj-box").style.display = ""
+  }
+};
+
+const fMasc = (objeto, mascara) => {
+  const obj = objeto
+  const masc = mascara
+  setTimeout(() => {
+    obj.value = masc(obj.value)
+  }, 1);
+};
+  
+const mCPF = cpf => {
+  cpf = cpf.replace(/\D/g,"")
+  cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf = cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+  return cpf
+}
 
 export { cpfOrCnpj };
+export { fMasc };
+export { mCPF };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,8 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initSweetalert } from '../plugins/init_sweetalert';
 import { cpfOrCnpj } from '../components/form-volunteer';
+import { fMasc } from '../components/form-volunteer';
+import { mCPF } from '../components/form-volunteer';
 // CSS
 import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
@@ -52,3 +54,6 @@ document.addEventListener('turbolinks:load', () => {
 
 document.getElementById("volunteer_cpf_or_cnpj_cnpj").addEventListener('click', cpfOrCnpj);
 document.getElementById("volunteer_cpf_or_cnpj_cpf").addEventListener('click', cpfOrCnpj);
+document.getElementById('volunteer_cpf').addEventListener('keydown', () => {
+  fMasc(document.getElementById('volunteer_cpf'), mCPF);
+});

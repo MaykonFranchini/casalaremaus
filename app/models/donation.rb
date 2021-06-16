b0e9ab6b968ec2a909d585a15c7f642c59308016
belisa bettega <belisabettega@gmail.com>
2021-06-16 09:52:33 -0300

    ajusted destroy method

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,5 @@
 class Donation < ApplicationRecord
+    has_many :items, dependent: :destroy
     validates :name, :quantity, :description, presence: true
     TAGS = ["Alimentos", "Higiene Pessoal", "Carnes", "Produtos de Limpeza", "Outro"]
 end

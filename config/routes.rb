4e60f5beb93d77f710766abe619b0bef19cfa586
belisa bettega <belisabettega@gmail.com>
2021-06-16 15:48:29 -0300

    implemented the orders dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'transfer', to: 'pages#transfer', as: :transfer
   get 'pages/download', as: :download
   get 'contact', to: 'pages#contact', as: :contact
-  resources :orders, except: [:index, :destroy] do
+  resources :orders, except: :index do
       get 'mailer', to: 'orders#mailer', as: :mailer
     resources :items, only: [:create, :destroy]
   end

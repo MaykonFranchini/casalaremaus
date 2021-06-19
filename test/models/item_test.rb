6a01af8047d07c80728178d3e4465a7142cf3890
belisa bettega <belisabettega@gmail.com>
2021-06-19 12:21:36 -0300

    created a test for the item model

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -4,4 +4,10 @@ class ItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save item without quantity" do
+    donation = Donation.create(name: "Rice", quantity: 13, description: "unidade")
+    order = Order.create(name:"Order order", email:"name@name.com", phone: "2154125")
+    item = Item.new(donation: donation, order: order)
+    assert_not item.save
+  end
 end

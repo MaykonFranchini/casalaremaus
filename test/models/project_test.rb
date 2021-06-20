1046acac62874abdc858bdc860881a2d31293300
belisa bettega <belisabettega@gmail.com>
2021-06-20 16:54:00 -0300

    ajusted the project test to include a correct project saving

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -5,9 +5,11 @@ class ProjectTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "should not save project without name and a description with length > 10" do
+    correct_project = Project.new(name: "Projeto de teste", description: "This is a description with length > 10")
     project_with_name = Project.new(name: "Projeto de teste")
     project_with_description = Project.new(description: "This is a description with length > 10")
     project_with_small_description = Project.new(description: "< 10")
+    assert correct_project.save
     assert_not project_with_name.save, "Saved the project without a description"
     assert_not project_with_description.save, "Saved the project without a name"
     assert_not project_with_small_description.save, "Saved the project with description length < 10"

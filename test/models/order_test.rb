e9a6c82e3fe27089b7834fc8694a8a43213bc3c1
belisa bettega <belisabettega@gmail.com>
2021-06-20 16:06:22 -0300

    created tests for the project model + ajusted method create inside order_test + commented the user mailer tests for now

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -5,9 +5,9 @@ class OrderTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "should not save order without name, email and phone" do
-    order_with_name = Order.create(name:"Order order")
-    order_with_email = Order.create(email:"name@name.com")
-    order_with_phone = Order.create(phone: "2154125")
+    order_with_name = Order.new(name:"Order order")
+    order_with_email = Order.new(email:"name@name.com")
+    order_with_phone = Order.new(phone: "2154125")
     assert_not order_with_name.save, "Saved the order without a email and phone"
     assert_not order_with_email.save, "Saved the order without a name and phone"
     assert_not order_with_phone.save, "Saved the order without a email and name"
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,4 +4,12 @@ class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save project without name and a description with length > 10" do
+    project_with_name = Project.new(name: "Projeto de teste")
+    project_with_description = Project.new(description: "This is a description with length > 10")
+    project_with_small_description = Project.new(description: "< 10")
+    assert_not project_with_name.save, "Saved the project without a description"
+    assert_not project_with_description.save, "Saved the project without a name"
+    assert_not project_with_small_description.save, "Saved the project with description length < 10"
+  end
 end

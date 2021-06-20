214951b81bb9ae4652efbfd476b3fdeda24baa63
belisa bettega <belisabettega@gmail.com>
2021-06-20 17:12:53 -0300

    ajusted the unity test for the order model

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,16 +1,15 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "should not save order without name, email and phone" do
-    order_with_name = Order.new(name:"Order order")
-    order_with_email = Order.new(email:"name@name.com")
-    order_with_phone = Order.new(phone: "2154125")
-    assert_not order_with_name.save, "Saved the order without a email and phone"
-    assert_not order_with_email.save, "Saved the order without a name and phone"
-    assert_not order_with_phone.save, "Saved the order without a email and name"
-    assert_equal false, order_with_name.status, "Order default status must be false"
+    correct_order = Order.new(name:"Order order", email:"name@name.com", phone: "2154125")
+    order_without_name = Order.new(email:"name@name.com", phone: "2154125")
+    order_without_email = Order.new(name:"Order order", phone: "2154125")
+    order_without_phone = Order.new(name:"Order order", email:"name@name.com")
+    assert correct_order.save
+    assert_not order_without_name.save, "Saved the order without a name"
+    assert_not order_without_email.save, "Saved the order without email"
+    assert_not order_without_phone.save, "Saved the order without a phone"
+    assert_equal false, correct_order.status, "Order default status must be false"
   end
 end

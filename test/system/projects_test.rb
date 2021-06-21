85f087ba9726c3110030853a64b576773537e70a
belisa bettega <belisabettega@gmail.com>
2021-06-21 14:14:02 -0300

    created fixture to projects

--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -3,7 +3,6 @@ require "application_system_test_case"
 class ProjectsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit projects_path
-  
     assert_selector "h2", text: "Nossos projetos disponíveis:"
   end
 end

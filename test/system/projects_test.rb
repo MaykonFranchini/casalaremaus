21eb1cce7e7122eb350541ca7fe1a9bf9df38f8a
belisa bettega <belisabettega@gmail.com>
2021-06-21 15:35:00 -0300

    added a system test for projects - problem:readTimeOut

--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -5,4 +5,15 @@ class ProjectsTest < ApplicationSystemTestCase
     visit projects_path
     assert_selector "h2", text: "Nossos projetos disponíveis:"
   end
+
+  test "lets a visitor create a new project" do
+    visit "/projects/new"
+
+    fill_in "project_name", with: "Le Wagon"
+    fill_in "project_description", with: "Change your life: Learn to code"
+
+    click_on 'Enviar minha proposta'
+
+    assert_equal root_path, page.current_path
+  end
 end
